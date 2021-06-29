686f6bfeca2e8bd850d0ce3137bca883cd9ae6cf
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2021-06-29 09:44:05 -0300

    Fix pubsub adapter

--- apps/ushortener/lib/ushortener/application.ex
+++ apps/ushortener/lib/ushortener/application.ex
@@ -8,7 +8,7 @@ defmodule Ushortener.Application do
   def start(_type, _args) do
     children = [
       Ushortener.Repo,
-      {Phoenix.PubSub, name: Ushortener.PubSub}
+      {Phoenix.PubSub, name: Ushortener.PubSub, adapter: Phoenix.PubSub.PG2}
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one, name: Ushortener.Supervisor)
